b360c7e35470d44d973af96245b672d29a80fbc6
liaochangjiang <changjiangthecoder@gmail.com>
2020-01-04 15:56:41 +0800

    feature: update demo

--- README.md
+++ README.md
@@ -10,3 +10,120 @@ Authing OIDC RP 端处理方式，基本示例。
 3. [OIDC 常见问题](https://docs.authing.cn/authing/advanced/oidc/oidc-params)
 
 若以上文档无法访问，请将域名改为 `learn.authing.cn`。
+
+## 如何运行
+
+```
+$ yarn
+$ node index.js
+```
+
+## Demo 及原理讲解
+
+### 基础配置
+
+```javascript
+const port = 8888
+const oidcAppId = "OIDC 应用 AppId";
+const oidcAppSecret = "OIDC 应用 AppSecret";
+const redirect_uri = `http://localhost:${port}/oidc/handle`
+```
+
+![](http://lcjim-img.oss-cn-beijing.aliyuncs.com/2020-01-04-073932.png)
+
+
+- oidcAppId 填入 OIDC 应用 AppId
+- oidcAppSecret 填入 OIDC 应用 AppSecret
+- 将 OIDC 应用的回调 URL 设置为本项目的 `/oidc/handle` 接口
+
+
+### 使用 Guard 登录获取 Code
+
+点击体验登录访问 Guard 在线地址
+
+![](http://lcjim-img.oss-cn-beijing.aliyuncs.com/2020-01-04-074203.png)
+
+
+![](http://lcjim-img.oss-cn-beijing.aliyuncs.com/2020-01-04-074319.png)
+
+成功登录之后，将会回调到本项目到 `/oidc/handle`，并且在 Get 请求参数中携带 code。
+
+### 使用 Code 换 Token
+
+```javascript
+const qs = require('querystring')
+
+code2tokenResponse = await axios.post(
+  "https://oauth.authing.cn/oauth/oidc/token",
+  qs.stringify({
+    code,
+    client_id: oidcAppId,
+    client_secret: oidcAppSecret,
+    grant_type: "authorization_code",
+    redirect_uri
+  }),
+  {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+);
+```
+
+有几点需要注意：
+- `Content-Type` 为 `application/x-www-form-urlencoded`，非 `application/json`
+- Post Body 要转成 `var1=xx&var2=xxx` 的格式。（`qs.stringify()`）
+
+
+### 使用 Token 换用户信息
+
+```javascript
+let token2UserInfoResponse = await axios.get("https://users.authing.cn/oauth/oidc/user/userinfo?access_token=" + access_token);
+```
+
+返回的标准 OIDC 用户信息格式如下：
+
+```json
+{
+  "sub": "5e0db2f0cfe70fe1e5ce0791",
+  "birthdate": "",
+  "family_name": "",
+  "gender": "",
+  "given_name": "",
+  "locale": "",
+  "middle_name": "",
+  "name": "",
+  "nickname": "廖长江",
+  "picture": "https://usercontents.authing.cn/avatar-5e0db2f0cfe70fe1e5ce0791-1577956080139",
+  "preferred_username": "",
+  "profile": "",
+  "updated_at": "",
+  "website": "",
+  "zoneinfo": ""
+}
+```
+
+### 携带 id_token 访问 protected resource
+
+```javascript
+let idToken = ctx.header.authorization || ctx.query.id_token;
+try {
+  // 使用 oidc 应用的 clientSecret 进行 token 验证
+  // 验证失败，jsonwebtoken 库会抛出错误，比如 token 过期，签名错误
+  let decoded = jwt.verify(idToken, oidcAppSecret);
+  ctx.body = {
+    decoded,
+    protected: "This is protected resource."
+  }
+} catch (err) {
+  // 把用户重定向到 oidc 授权地址，进行登录
+  ctx.redirect(
+    `http://oauth.authing.cn/oauth/oidc/auth?client_id=${oidcAppId}&redirect_uri=${redirect_uri}&scope=openid%20profile%20offline_access%20phone%20email&response_type=code&state=jazzb&nonce=22121&prompt=consent`
+  );
+}
+```
+
+- 从 `authorization` 请求头或 `id_token` query 中获取 id_token
+- 使用 `oidcAppSecret` 尝试解密
+  - 如果成功，说明已经登录，可以返回 protected resource
+  - 如果失败，跳转到 `http://oauth.authing.cn/oauth/oidc/auth?client_id=${oidcAppId}&redirect_uri=${redirect_uri}&scope=openid%20profile%20offline_access%20phone%20email&response_type=code&state=jazzb&nonce=22121&prompt=consent`，也即上面提到的登录表单页面。
--- index.js
+++ index.js
@@ -3,51 +3,77 @@ const koaRouter = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
-let app = new koa();
-let router = new koaRouter();
-const clientId = "aaa";
-const clientSecret = "xxx";
+const qs = require('querystring')
+const app = new koa();
+const router = new koaRouter();
+
+const port = 8888
+const oidcAppId = "OIDC 应用 AppId";
+const oidcAppSecret = "OIDC 应用 AppSecret";
+const redirect_uri = `http://localhost:${port}/oidc/handle`
+
 // Authing 控制台 redirect_uri 可以填下面这个。本示例 code 换 token，token 换用户信息都在后端完成。code 由 Authing 以 url query 的形式发到 redirect_uri。
 router.get("/oidc/handle", async (ctx, next) => {
   let code = ctx.query.code;
   // code 换 token
-  let res = await axios.post(
-    "https://yourapp.authing.cn/oauth/oidc/token",
-    {
-      code,
-      client_id: clientId,
-      client_secret: clientSecret,
-      grant_type: "authorization_code",
-      redirect_uri: "https://yourapp.cn/oidc/handle"
-    }
-  );
-  let { access_token, id_token } = res.data;
+  let code2tokenResponse
+  try {
+    code2tokenResponse = await axios.post(
+      "https://oauth.authing.cn/oauth/oidc/token",
+      qs.stringify({
+        code,
+        client_id: oidcAppId,
+        client_secret: oidcAppSecret,
+        grant_type: "authorization_code",
+        redirect_uri
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+    );
+  } catch (error) {
+    ctx.body = error.response.data
+    return
+  }
+
+  let { access_token, id_token } = code2tokenResponse.data;
   // token 换用户信息
-  let userInfo = await axios.get(
-    "https://users.authing.cn/oauth/oidc/user/userinfo?access_token" +
-      access_token
-  );
+  let token2UserInfoResponse = await axios.get("https://users.authing.cn/oauth/oidc/user/userinfo?access_token=" + access_token);
+
   // 这里可以操作用户信息，比如存入数据库
   // ...
   // 把用户重定向到前端登录处理页，携带 id_token，前端需要把这里的 id_token 保存，以后访问受保护资源时要携带
-  ctx.redirect("http://localhost:4000/login/handle?id_token=" + id_token);
+
+  ctx.body = {
+    'code -> access_token response': code2tokenResponse.data,
+    'access_token -> userInfo response': token2UserInfoResponse.data
+  }
 });
+
+
 router.get("/protected/resource", async (ctx, next) => {
   // 用户访问受保护资源需要携带 id_token
-  let idToken = ctx.header.authorization;
+  let idToken = ctx.header.authorization || ctx.query.id_token;
   try {
     // 使用 oidc 应用的 clientSecret 进行 token 验证
     // 验证失败，jsonwebtoken 库会抛出错误，比如 token 过期，签名错误
-    let decoded = jwt.verify(idToken, clientSecret);
-    ctx.body = {protected: "This is protected resource."}
+    let decoded = jwt.verify(idToken, oidcAppSecret);
+    ctx.body = {
+      decoded,
+      protected: "This is protected resource."
+    }
   } catch (err) {
     // 把用户重定向到 oidc 授权地址，进行登录
     ctx.redirect(
-      `http://yourapp.authing.cn/oauth/oidc/auth?client_id=${clientId}&redirect_uri=https://yourapp.cn/oidc/handle&scope=openid%20profile%20offline_access%20phone%20email&response_type=code&state=jazzb&nonce=22121&prompt=consent`
+      `http://oauth.authing.cn/oauth/oidc/auth?client_id=${oidcAppId}&redirect_uri=${redirect_uri}&scope=openid%20profile%20offline_access%20phone%20email&response_type=code&state=jazzb&nonce=22121&prompt=consent`
     );
   }
 });
+
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.listen(3000, "localhost");
+app.listen(port);
+console.log(`App listening at http://localhost:${port}`)
